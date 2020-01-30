6af831960d96af52ddef869af7d01c850c087e3e
Venelina11 <60343322+Venelina11@users.noreply.github.com>
2020-01-30 23:30:02 +0200

    Add another one -developments-

---
+++ developments.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class='part3'>
+
+<h1 class="article_header">Later Developments - After Abstract Expressionism</h1>
+<div class="image-container"><img src="images/4cf32072e09af6e6f550dd134128f8c2.jpg" 
+   style=" margin:auto; width:500px; height:500px; border:none;"
+   alt="photo of Jackson Pollock"
+   title="photo of Jackson Pollock" />
+
+
+   <p><strong>By the mid 1950s the style had also run its course in other ways. The movement's greatest achievements were often built on a conflict between chaos and control. which could only be played out in so many ways. Some artists, such as Newman and Rothko, had evolved a style so reductive that there was little room for development - and to change course would have shrunk the grandeur of their bold trademarks.</strong></p>
+<p><strong>Younger artists following the development of this generation were less persuaded by artists who were said to put forth one sublime expression after another, often in series, and they grew tired of their postures of heroism. Homosexual artists, such as Jasper Johns, Robert Rauschenberg,Andy Warhol, and Ellsworth Kelly, also felt little affinity with the macho styles and rhetoric of the New York School. Some, like Johns, would learn much from the Abstract Expressionists, and carry their interest in the autographic gesture in fresh directions, introducing qualities of irony, ambiguity and reticence, which the older generation could never have countenanced. Others, like Warhol, were too enthralled by the pop culture of the streets to have much in common with the lofty ambitions of hard-drinking womanizers such as Pollock and de Kooning.</strong></p>
+<p><strong>By the late 1950s, Abstract Expressionism had entirely lost its place at the center of critical debate and a new generation was on the cusp of success. Yet the legacy of the movement was to be considerable. Allan Kaprowsensed this as early as 1958 when he wrote an article for ART News entitled "What is the legacy of Jackson Pollock?" His answer pointed beyond painting, and Pollock's influence was certainly felt in areas where performance had a role: he was to be important to the Japanese Gutai movement as well as the Viennese Actionists. But the influence of the movement as a whole would continue to be felt by painters maturing in subsequent decades. It was important for the likes of -rockburne_dorothea"&gt;Dorothea Rockburne, Susan Rothenberg and Jack Whitten in the 1970s. Its rhetoric - if not its direct example - would be important for many Neo-Expressionists in the 1980s such as Julian Schnabel and Jean-Michel Basquiat. And in the 1990s it again provided an example to painters such as Cecily Brown. The themes and concepts that informed Abstract Expressionism may have lost the power to compel young artists, but the movement's achievements continue to supply them with standards against which to be measured.</strong></p>
+<p><strong>In 2016, the ladies of the movement finally received their due when the Denver Art Museum compiled the traveling Women of Abstraction exhibition. It was the first major organized recognition of over fifty important pieces seen together as a cohesive w>hole.</strong></p
+</div>
+</body>
+</html>
+
