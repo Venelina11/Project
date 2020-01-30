f34d6b92d712468e870e3ddb2269253b21e966e1
Venelina11 <60343322+Venelina11@users.noreply.github.com>
2020-01-30 21:51:50 +0200

    Add inproved and final  index

--- index.php
+++ index.php
@@ -9,39 +9,22 @@
                     <header>
                         <nav id="navigation">
                             <div>
-                                <li><a href="beginings.php">Beginnings</a></li>
+                                <li><a href="beginnings.php">Beginnings</a></li>
                                 <li><a href="concepts.php">Concepts</a></li>
-                                <li><a href="later.php">Developments</a></li>
+                                <li><a href="developments.php">Developments</a></li>
+                                <li><a href="important.php">Most important art</a></li>
                                 <li><a href="upload.php">Upload additional information</a></li>
                                 
-                                
-                                <h1>Abstract Expressionism</h1>
-
-                                <h3>Summary of Abstract Expressionism</h3>
-<p class="article_text">&quot;Abstract Expressionism&quot; was never an ideal label for the movement, which developed in New York in the 1940s and 1950s. It was somehow meant to encompass not only the work of painters who filled their canvases with fields of color and abstract forms, but also those who attacked their canvases with a vigorous gestural expressionism. Still Abstract Expressionism has become the most accepted term for a group of artists who held much in common. All were committed to art as expressions of the self, born out of profound emotion and universal themes, and most were shaped by the legacy of Surrealism, a movement that they translated into a new style fitted to the post-war mood of anxiety and trauma. In their success, these New York painters robbed Paris of its mantle as leader of modern art, and set the stage for America&#39;s dominance of the international art world.</p>
-<p>&nbsp;</p>
-<h3>
-	Key Ideas</h3>
+<div class="bla">
+<br><br><br><br><br><h2>Summary of Abstract Expressionism</h2>
+<p><strong>"Abstract Expressionism" was never an ideal label for the movement, which developed in New York in the 1940s and 1950s. It was somehow meant to encompass not only the work of painters who filled their canvases with fields of color and abstract forms, but also those who attacked their canvases with a vigorous gestural expressionism. Still Abstract Expressionism has become the most accepted term for a group of artists who held much in common. All were committed to art as expressions of the self, born out of profound emotion and universal themes, and most were shaped by the legacy of Surrealism, a movement that they translated into a new style fitted to the post-war mood of anxiety and trauma. In their success, these New York painters robbed Paris of its mantle as leader of modern art, and set the stage for America's dominance of the international art world.</strong></p>
+<h3>Key Ideas</h3>
+</div>
 <div class="article_list">
-	<ul>
-	<li style="text-align: justify;">Political instability in Europe in the 1930s brought several leading Surrealists to New York, and many of the Abstract Expressionists were profoundly influenced by Surrealism&#39;s focus on mining the unconscious. It encouraged their interest in myth and archetypal symbols and it shaped their understanding of painting itself as a struggle between self-expression and the chaos of the subconscious.<br />
-	&nbsp;</li>
-	<li style="text-align: justify;">Most of the artists associated with Abstract Expressionism matured in the 1930s. They were influenced by the era&#39;s leftist politics, and came to value an art grounded in personal experience. Few would maintain their earlier radical political views, but many continued to adopt the posture of outspoken <i>avant-gardists</i>.<br />
-	&nbsp;</li>
-	<li style="text-align: justify;">Having matured as artists at a time when America suffered economically and felt culturally isolated and provincial, the Abstract Expressionists were later welcomed as the first authentically American <i>avant-garde</i>. Their art was championed for being emphatically American in spirit - monumental in scale, romantic in mood, and expressive of a rugged individual freedom.<br />
-	&nbsp;</li>
-	<li style="text-align: justify;">Although the movement has been largely depicted throughout historical documentation as one belonging to the paint-splattered, heroic male artist, there were several important female Abstract Expressionists that arose out of New York and San Francisco during the 1940s and &#39;50s who now receive credit as elemental members of the canon.</li>
-	</ul>
-    </div>
-
-                                </body>
-                            </head>
-                            </ul>
-                        </nav>
-                    </header>
-                    <div id="contents">
-
-
-                
-
-
+<ul style="list-style-type: disc;">
+<li><strong>Political instability in Europe in the 1930s brought several leading Surrealists to New York, and many of the Abstract Expressionists were profoundly influenced by Surrealism's focus on mining the unconscious. It encouraged their interest in myth and archetypal symbols and it shaped their understanding of painting itself as a struggle between self-expression and the chaos of the subconscious.</strong><br /><strong> &nbsp;</strong></li>
+<li><strong>Most of the artists associated with Abstract Expressionism matured in the 1930s. They were influenced by the era's leftist politics, and came to value an art grounded in personal experience. Few would maintain their earlier radical political views, but many continued to adopt the posture of outspoken <em>avant-gardists</em>.</strong><br /><strong> &nbsp;</strong></li>
+<li><strong>Having matured as artists at a time when America suffered economically and felt culturally isolated and provincial, the Abstract Expressionists were later welcomed as the first authentically American <em>avant-garde</em>. Their art was championed for being emphatically American in spirit - monumental in scale, romantic in mood, and expressive of a rugged individual freedom.</strong><br /><strong> &nbsp;</strong></li>
+<li><strong>Although the movement has been largely depicted throughout historical documentation as one belonging to the paint-splattered, heroic male artist, there were several important female Abstract Expressionists that arose out of New York and San Francisco during the 1940s and '50s who now receive credit as elemental members of the canon.</strong></li>
+</ul>
+</div>
